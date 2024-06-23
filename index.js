6b5266157c085033754e58fc24070db330e6ac97
vaibhav1724 <153760704+vaibhav1724@users.noreply.github.com>
2024-06-24 02:53:32 +0530

    backend done

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
+const { default: mongoose } = require("mongoose");
 const app = express()
 
 app.use(express.json())
 
-app.post('/todo', (req,res)=>{
+app.post('/todo', async (req,res)=>{
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
     if(!parsedPayload.success){
@@ -13,16 +15,24 @@ app.post('/todo', (req,res)=>{
         })
         return
     }
-    res.status(200).json({
-        msg : "the todo has been added"
+    await todo.create({
+        title : createPayload.title,
+        description : createPayload.description,
+        completed : false
+    })
+    res.json({
+        msg : " todo added"
     })
 })
 
-app.get('/todos', (req,res)=>{
-
+app.get('/todos',async (req,res)=>{
+    todos = await todo.find({})
+    res.json({
+        todos 
+    })
 })
 
-app.put('/completed', (req,res)=>{
+app.put('/completed', async (req,res)=>{
     const updatePayload = req.body
     const parsedPayload = updateTodo.safeParse(updatePayload)
     if(!parsedPayload.success){
@@ -31,5 +41,15 @@ app.put('/completed', (req,res)=>{
         })
         return
     }
+    await todo.update({
+        _id : req.body.id
+    }, {
+        completed : true
+    })
+    res.json({
+        msg : "Todos mark as completed"
+    })
+    
 })
 
+app.listen(300)
