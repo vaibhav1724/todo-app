cc2af089532e09673dc54fee360fb19ec790bf6f
vaibhav1724 <153760704+vaibhav1724@users.noreply.github.com>
2024-06-24 02:10:25 +0530

    created backend

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { createTodo, updateTodo } = require("./types");
+const app = express()
+
+app.use(express.json())
+
+app.post('/todo', (req,res)=>{
+    const createPayload = req.body;
+    const parsedPayload = createTodo.safeParse(createPayload);
+    if(!parsedPayload.success){
+        res.status(411).json({
+            msg : "You sent wrong inputs"
+        })
+        return
+    }
+    res.status(200).json({
+        msg : "the todo has been added"
+    })
+})
+
+app.get('/todos', (req,res)=>{
+
+})
+
+app.put('/completed', (req,res)=>{
+    const updatePayload = req.body
+    const parsedPayload = updateTodo.safeParse(updatePayload)
+})
+
