768f847b5cb2417224373011d60391769185c33a
vaibhav1724 <153760704+vaibhav1724@users.noreply.github.com>
2024-06-24 03:00:14 +0530

    resolved db.js issues

--- db.js
+++ db.js
@@ -1,8 +1,8 @@
-const mongoose = require("monsgoose")
+const mongoose = require("mongoose")
 // this goes into .env file but just for the project
 mongoose.connect('mongodb+srv://admin:12345@cluster0.frfluap.mongodb.net/todos');
 
-const todoSchema = mongoose.todoSchema({
+const todoSchema = mongoose.Schema({
     title : String,
     description : String,
     completed : Boolean
